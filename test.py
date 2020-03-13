14f4638511e5bc706234188854eada3928c20688
wybosys <wybosys@gmail.com>
2020-03-13 17:41:58 +0800

    init

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-import signals
+from signals import *
 
 def test():
-    pass
+    a = SObject()
+    a.signals.register("a")
+    a.signals.connect("a",
+        lambda s:
+            print("signal a: lambda")
+        )
+    
+    a.signals.emit("a")    
 
 if __name__ == "__main__":
     test()
-    
